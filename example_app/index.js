89355113107b1d46b462ed66a12c53ee3de88411
John Aldrich Bernardo <4ldrich@protonmail.com>
2023-03-05 20:06:32 +0800

    adding example app

---
+++ example_app/index.js
@@ -0,0 +1,31 @@
+import mysql from 'mysql';
+import util from 'util';
+
+const masterConnection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "mydb_user",
+  password: "mydb_pwd",
+  port: 4406,
+  database: "mydb",
+});
+
+const slaveConnection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "mydb_slave_user",
+  password: "mydb_slave_pwd",
+  port: 5506,
+  database: "mydb",
+});
+
+(async () => {
+  const masterQuery = util.promisify(masterConnection.query).bind(masterConnection);
+  const masterResults = await masterQuery("INSERT INTO code VALUES (?)", [1002]);
+  console.log(masterResults);
+  masterConnection.end();
+
+  const slaveQuery = util.promisify(slaveConnection.query).bind(slaveConnection);
+  const slaveResults = await slaveQuery("SELECT * FROM code");
+  slaveConnection.end();
+
+  console.log(slaveResults);
+})()
